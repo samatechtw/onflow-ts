a6aca5a11e517c8ccbdd882d471d713938ee6c2b
Ilia Nazarov <inazarov@colddata.com>
2022-03-07 14:30:39 +0100

    feat: add function to transfer FUSDs from one account to another #18

--- lib/node/fusd.ts
+++ lib/node/fusd.ts
@@ -4,10 +4,22 @@ import { IFlowAccount } from '../type'
 import { deploy, transact, execute } from './interactions'
 import { toUFix64 } from '../shared/misc'
 
+/*
+ * Deploys FUSD contract to the account
+ * @param {IFlowAccount} account - the account to deploy the FUSD contract on
+ * @throws If execution will be halted
+ * @returns {Promise<*>}
+ * */
 export const deployFusd = async (account: IFlowAccount) => {
   await deploy({ name: 'FUSD', auth: account })
 }
 
+/*
+ * Sets up FUSD vault and capabilities to receive FUSDs and check the balance
+ * @param {IFlowAccount} account - the account to enable FUSDs on
+ * @throws If execution will be halted
+ * @returns {Promise<*>}
+ * */
 export const setupFusdOnAccount = (account: IFlowAccount) => {
   const name = 'fusd/setup_account'
   const signers = [account]
@@ -17,14 +29,22 @@ export const setupFusdOnAccount = (account: IFlowAccount) => {
   })
 }
 
+/*
+ * Mints FUSD tokens to the recipient account
+ * @param {IFlowAccount} admin - FUSD token admin
+ * @param {IFlowAccount} recipient - the account to receive FUSD once minted
+ * @param {UFix64} amount - amount of FUSD tokens to receive
+ * @throws If execution will be halted
+ * @returns {Promise<*>}
+ * */
 export const mintFusdToAccount = async (
   admin: IFlowAccount,
-  toAccount: IFlowAccount,
+  recipient: IFlowAccount,
   amount: number,
 ) => {
   const name = 'fusd/mint_tokens'
   const signers = [admin]
-  const args = [arg(toAccount.address, t.Address), arg(toUFix64(amount), t.UFix64)]
+  const args = [arg(recipient.address, t.Address), arg(toUFix64(amount), t.UFix64)]
 
   return transact({
     name,
@@ -33,9 +53,35 @@ export const mintFusdToAccount = async (
   })
 }
 
+/*
+ * Retrieves the FUSD tokens balance on the account
+ * @param {IFlowAccount} account - the account which balance to check
+ * @throws If execution will be halted
+ * @returns {Promise<*>}
+ * */
 export const getFusdBalance = async (account: IFlowAccount) => {
   const name = 'fusd/get_balance'
   const args = [arg(account.address, t.Address)]
 
   return execute(name, args)
+}
+
+/*
+ * Transfers FUSD tokens to recipient's account
+ * @param {IFlowAccount} recipient - transfer recipient
+ * @param {UFix64} amount - amount of FUSD tokens to transfer
+ * @param {IFlowAccount} signer - transaction signer (or sender)
+ * @throws If execution will be halted
+ * @returns {Promise<*>}
+ * */
+export const transferFusd = async (
+  amount: number,
+  recipient: IFlowAccount,
+  signer: IFlowAccount,
+) => {
+  const name = 'fusd/transfer_tokens'
+  const args = [arg(toUFix64(amount), t.UFix64), arg(recipient.address, t.Address)]
+  const signers = [signer]
+
+  return transact({ name, args, signers })
 }
