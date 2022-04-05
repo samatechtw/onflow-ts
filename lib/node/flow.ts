c113bf52d7e4b864ef269eb520ad35dc1fce9b1d
Sam Pullman <sampullman@gmail.com>
2022-04-05 20:25:46 +0800

    feat: add flow transfer and getbalance functions

--- lib/node/flow.ts
+++ lib/node/flow.ts
@@ -1,9 +1,13 @@
 import * as t from '@onflow/types'
 import { arg, Argument } from '@samatech/onflow-fcl-esm'
-import { IFlowAccount } from '../type'
-import { sendTransaction, replaceImportAddresses } from '../shared/interactions'
-import { withPrefix } from '../shared/misc'
-import { authorization } from '../shared/flow-crypto'
+import {
+  authorization,
+  executeScript,
+  replaceImportAddresses,
+  sendTransaction,
+  withPrefix,
+} from '../esm'
+import { IFlowAccount, ITransactionInteractionProps } from '../type'
 
 export const MINT_FLOW_CODE = `
 import FungibleToken from 0xFUNGIBLETOKENADDRESS
@@ -35,6 +39,56 @@ transaction(recipient: Address, amount: UFix64) {
 }
 `
 
+export const TRANSFER_FLOW_CODE = `
+import FungibleToken from 0xFUNGIBLETOKENADDRESS
+import FlowToken from 0xTOKENADDRESS
+
+transaction(amount: UFix64, to: Address) {
+
+    // The Vault resource that holds the tokens that are being transferred
+    let sentVault: @FungibleToken.Vault
+
+    prepare(signer: AuthAccount) {
+
+        // Get a reference to the signer's stored vault
+        let vaultRef = signer.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
+			?? panic("Could not borrow reference to the owner's Vault!")
+
+        // Withdraw tokens from the signer's stored vault
+        self.sentVault <- vaultRef.withdraw(amount: amount)
+    }
+
+    execute {
+
+        // Get the recipient's public account object
+        let recipient = getAccount(to)
+
+        // Get a reference to the recipient's Receiver
+        let receiverRef = recipient.getCapability(/public/flowTokenReceiver)
+            .borrow<&{FungibleToken.Receiver}>()
+			?? panic("Could not borrow receiver reference to the recipient's Vault")
+
+        // Deposit the withdrawn tokens in the recipient's receiver
+        receiverRef.deposit(from: <-self.sentVault)
+    }
+}
+`
+
+export const GET_FLOW_BALANCE_CODE = `
+import FungibleToken from 0xFUNGIBLETOKENADDRESS
+import FlowToken from 0xTOKENADDRESS
+
+pub fun main(account: Address): UFix64 {
+
+    let vaultRef = getAccount(account)
+        .getCapability(/public/flowTokenBalance)
+        .borrow<&FlowToken.Vault{FungibleToken.Balance}>()
+        ?? panic("Could not borrow Balance reference to the Vault")
+
+    return vaultRef.balance
+}
+`
+
 /**
  * Sends transaction to mint specified amount of FlowToken and send it to recipient.
  * Returns result of the transaction.
@@ -56,4 +110,53 @@ export const mintFlow = async (
   const auth = authorization(admin)
 
   return sendTransaction({ code, args, auth })
+}
+
+/**
+ * Transfer a specified amount of FlowToken to recipient.
+ * Returns result of the transaction.
+ * @param {string} sender - authorizing account
+ * @param {string} to - recipient account
+ * @param {string} amount - amount to transfer
+ * @returns {Promise<*>}
+ */
+export const transferFlow = async (
+  sender: IFlowAccount,
+  to: IFlowAccount,
+  amount: string,
+  props?: Partial<ITransactionInteractionProps>,
+) => {
+  const code = await replaceImportAddresses(TRANSFER_FLOW_CODE)
+  const args: Argument[] = [arg(amount, t.UFix64), arg(withPrefix(to.address), t.Address)]
+  const auth = authorization(sender)
+
+  return sendTransaction({ ...(props ?? {}), code, args, auth })
+}
+
+/**
+ * Gets the amount of FlowToken owned by an account
+ * Returns result of the transaction.
+ * @param {string} address - account address
+ * @returns {Promise<*>}
+ */
+export const getFlowBalanceCdc = async (address: string) => {
+  const code = await replaceImportAddresses(GET_FLOW_BALANCE_CODE)
+  const args: Argument[] = [arg(withPrefix(address), t.Address)]
+  return executeScript({ code, args }) as unknown as string
+}
+
+/**
+ * Returns FlowToken balance for an account
+ *
+ * @param address - account address
+ * @returns Account's FlowToken balance
+ */
+export const getFlowBalance = async (address: string): Promise<number> => {
+  const balanceUfix = await getFlowBalanceCdc(address)
+  const balance = parseFloat(balanceUfix)
+
+  if (isNaN(balance)) {
+    throw new Error(`Flow balance is not a number: ${balanceUfix}`)
+  }
+  return balance
 }
