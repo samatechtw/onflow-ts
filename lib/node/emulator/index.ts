9450024145c6ad08e9999331cc1d7c29edd8f383
Sam Pullman <sampullman@gmail.com>
2022-09-30 00:11:02 +0800

    refactor: move account manager to emulator folder

--- lib/node/emulator/index.ts
+++ lib/node/emulator/index.ts
@@ -1,3 +1,4 @@
 export * from './emulator'
 export * from './log-filter'
 export * from './test-setup'
+export * from './test-accounts'
--- lib/node/test/index.ts
+++ lib/node/test/index.ts
@@ -1,3 +1,2 @@
 export * from './jest-helpers'
-export * from './test-accounts'
 export * from './noop'
